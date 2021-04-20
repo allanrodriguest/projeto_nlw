768cc469c093e4411cd14226cdbe3c1d2de96018
Allan Rodrigues <60073178+allanrodriguest@users.noreply.github.com>
2021-04-20 08:54:19 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (request, response) => {
+    return response.json({
+        message:"HELLO NLW 05",
+    });
+});
+
+app.post("/", (request, response) => {
+    return response.json({ 
+        message: "Usuario salvo com sucesso!"});
+})
+
+
+app.listen(3333, () => console.log("Server is running on port 3333."));
