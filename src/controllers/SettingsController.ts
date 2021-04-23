aadad322e4ddf1a439cc2be536a03833c74f2d6e
Allan Rodrigues <60073178+allanrodriguest@users.noreply.github.com>
2021-04-22 22:42:38 -0300

    Third Commit

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository"
+import { SettingsService } from "../services/SettingsService";
 
 
 class SettingsController {
@@ -8,17 +7,18 @@ class SettingsController {
     async create(request: Request, response: Response){
         const { chat, username } = request.body;
     
-        const settingsRepository = getCustomRepository(SettingsRepository);
-    
-        const settings = settingsRepository.create({
-            chat,
-            username
-        });
-    
-        await settingsRepository.save(settings);
+        const settingsService = new SettingsService();
+
+        try {
+        const settings = await settingsService.create({chat, username})
     
         return response.json(settings);  
+    } catch(err) {
+        return response.status(400).json({
+            message: err.message,
+        });
     }
+}
 }
 
 export { SettingsController };
