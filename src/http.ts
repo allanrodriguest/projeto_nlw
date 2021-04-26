4f307a7106e17f2213dffd652be8dbe217af148b
Allan Rodrigues <60073178+allanrodriguest@users.noreply.github.com>
2021-04-26 11:18:37 -0300

    Http & Server Updated

--- src/http.ts
+++ src/http.ts
@@ -1,32 +1,35 @@
-import express from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-import path from "path";
+import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
 
-import "./database";
-import { routes } from "./routes";
+import { routes } from './routes';
+
+import './database';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.set("views", path.join(__dirname, "..", "public"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
-app.get("/pages/client", (request, response) => {
-    return response.render("html/client.html")
-})
+app.get('/pages/client', (req, res) => {
+  return res.render('html/client.html');
+});
+
+app.get('/pages/admin', (req, res) => {
+  return res.render('html/admin.html');
+});
 
 const http = createServer(app);
-
 const io = new Server(http);
 
-io.on("connection", (socket: Socket) => {
-   // console.log("Connected!", socket.id);
+io.on('connection', (socket: Socket) => {
+  console.log('Se conectou', socket.id);
 });
 
 app.use(express.json());
-
 app.use(routes);
 
-export { http, io};
+export { http, io };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import { http } from "./http";
-import "./websocket/client";
+import { http } from './http';
+import './websocket/client';
+import './websocket/admin';
 
-http.listen(3333, () => console.log("Server is running on port 3333."));
+http.listen(3333, () => console.log('Server started on 3333'));
